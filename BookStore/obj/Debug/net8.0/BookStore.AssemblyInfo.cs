480e53d10300f28c06f4b5060e76664a4583ae78
Nguyên <nguyentrungnguyen1945@gmail.com>
2025-12-22 15:04:23 +0700

    first commit

--- BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7905100e17665e360adbc5825b8a6c674d45926")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceddbfb70e57cce6d9c7c41182664bf5f779c163")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
