9095355da854eadc4011ece962f6c66870df15bb
Nguyên <nguyentrungnguyen1945@gmail.com>
2025-12-29 11:55:41 +0700

    them,

--- BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d1c5a3684cbf69a83aac81058e1cd60efd316da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e87cd6fb2e4f7184ce56c9b4a912412682797f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
